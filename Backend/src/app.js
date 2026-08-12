466aef9bd92c89bd653379d8fb5ab3810bbce439
Mehraj Sayyad <sayyadmehraj01@gmail.com>
2026-08-12 17:38:23 +0530

    Cors error is handled in Backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,9 @@ import express from "express";
 import authRouter from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
 
+//To handle cors error
+import cors from "cors"
+
 // creates your Express application.
 const app = express();
 
@@ -11,6 +14,13 @@ app.use(express.json())
 
 // Lets your server read cookies from incoming HTTP requests.  
 app.use(cookieParser())
+
+//Handle cors error
+//Origin is the frontend url, credentials:true means we are allowing cookies to be sent from frontend to backend
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 
 //All the auth related api's will be directed to authRouter file
 app.use("/api/auth",authRouter)
