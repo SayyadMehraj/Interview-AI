5b2f5cf867e3c1dd75bba71048e1d34dce325bdc
Mehraj Sayyad <sayyadmehraj01@gmail.com>
2026-08-12 13:38:54 +0530

    Created the Login & Register UI Interface and also their routes

---
+++ Frontend/src/app.routes.jsx
@@ -0,0 +1,16 @@
+import {createBrowserRouter} from "react-router"
+import Login from "./features/auth/pages/Login.jsx"
+import Register from "./features/auth/pages/Register.jsx"
+
+
+export const router = createBrowserRouter([
+    {
+        //What is the route
+        path:"/login",
+        element:<Login/>
+    },
+    {
+        path:"/register",
+        element:<Register/>
+    }
+])
